6dc2f8390f663d87e6c4b4e0f179df85e848291e
Abdeljalil Elhachimi <eabduljalil@outlook.com>
2022-01-14 00:49:30 +0100

    some colorschemes

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -60,7 +60,7 @@ return packer.startup(function(use)
   use "folke/which-key.nvim"
 
   -- Colorschemes
-  -- use "lunarvim/colorschemes" -- A bunch of colorschemes you can try out
+  use "lunarvim/colorschemes" -- A bunch of colorschemes you can try out
   use "lunarvim/darkplus.nvim"
 
   -- cmp plugins
