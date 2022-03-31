d0b4f49532c4a64d0385ff75b93a477ae4960314
Abdeljalil Elhachimi <eabduljalil@outlook.com>
2022-03-31 20:23:20 +0000

    switch to tokyonight's night style

--- nvim/lua/user/colorscheme.lua
+++ nvim/lua/user/colorscheme.lua
@@ -1,6 +1,7 @@
 -- Example config in Lua
 vim.g.tokyonight_italic_functions = true
 vim.g.tokyonight_sidebars = { "qf", "vista_kind", "terminal", "packer" }
+vim.g.tokyonight_style = "night"
 
 -- Change the "hint" color to the "orange" color, and make the "error" color bright red
  vim.g.tokyonight_colors = { hint = "orange", error = "#ff0000" }
