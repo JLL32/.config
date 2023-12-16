739584d7b1b893c6bcf734e4bded147a5c921057
JLL32 <eabduljalil@outlook.com>
2023-12-16 21:30:03 +0100

    add neovide config

--- nvim/init.lua
+++ nvim/init.lua
@@ -132,6 +132,32 @@ require('lazy').setup({
       build = ':TSUpdate',
    },
 
+   {
+      "piersolenski/wtf.nvim",
+      dependencies = {
+         "MunifTanjim/nui.nvim",
+      },
+      opts = {},
+      keys = {
+         {
+            "gw",
+            mode = { "n", "x" },
+            function()
+               require("wtf").ai()
+            end,
+            desc = "Debug diagnostic with AI",
+         },
+         {
+            mode = { "n" },
+            "gW",
+            function()
+               require("wtf").search()
+            end,
+            desc = "Search diagnostic with Google",
+         },
+      },
+   },
+
    -- NOTE: Next Step on Your Neovim Journey: Add/Configure additional "plugins" for kickstart
    --       These are some example plugins that I've included in the kickstart repository.
    --       Uncomment any of the lines below to enable them.
@@ -445,26 +471,26 @@ local options = {
    smartindent = true,                      -- make indenting smarter again
    smarttab = true,
    autoindent = true,
-   splitbelow = true,                  -- force all horizontal splits to go below current window
-   splitright = true,                  -- force all vertical splits to go to the right of current window
-   swapfile = false,                   -- creates a swapfile
-   timeoutlen = 300,                   -- time to wait for a mapped sequence to complete (in milliseconds)
-   undofile = true,                    -- enable persistent undo
-   updatetime = 250,                   -- faster completion (4000ms default)
-   writebackup = false,                -- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
-   expandtab = true,                   -- convert tabs to spaces
-   shiftwidth = 3,                     -- the number of spaces inserted for each indentation
-   tabstop = 3,                        -- insert 2 spaces for a tab
-   cursorline = true,                  -- highlight the current line
-   number = true,                      -- set numbered lines
-   relativenumber = true,              -- set relative numbered lines
-   numberwidth = 4,                    -- set number column width to 2 {default 4}
-   signcolumn = "yes",                 -- always show the sign column, otherwise it would shift the text each time
-   wrap = false,                       -- display lines as one long line
-   scrolloff = 8,                      -- is one of my fav
+   splitbelow = true,                     -- force all horizontal splits to go below current window
+   splitright = true,                     -- force all vertical splits to go to the right of current window
+   swapfile = false,                      -- creates a swapfile
+   timeoutlen = 300,                      -- time to wait for a mapped sequence to complete (in milliseconds)
+   undofile = true,                       -- enable persistent undo
+   updatetime = 250,                      -- faster completion (4000ms default)
+   writebackup = false,                   -- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
+   expandtab = true,                      -- convert tabs to spaces
+   shiftwidth = 3,                        -- the number of spaces inserted for each indentation
+   tabstop = 3,                           -- insert 2 spaces for a tab
+   cursorline = true,                     -- highlight the current line
+   number = true,                         -- set numbered lines
+   relativenumber = true,                 -- set relative numbered lines
+   numberwidth = 4,                       -- set number column width to 2 {default 4}
+   signcolumn = "yes",                    -- always show the sign column, otherwise it would shift the text each time
+   wrap = false,                          -- display lines as one long line
+   scrolloff = 8,                         -- is one of my fav
    sidescrolloff = 8,
-   breakindent = true,                 -- Enable break indent
-   guifont = "FiraCode Nerd Font:h14", -- the font used in graphical neovim applications
+   breakindent = true,                    -- Enable break indent
+   guifont = "Monoid Nerd Font Mono:h13", -- the font used in graphical neovim applications
    listchars = {
       tab = '→ ',
       extends = '⟩',
@@ -496,3 +522,14 @@ end
 
 -- Create a command that maps to the function
 vim.cmd('command! VerticalTerms lua OpenVerticalTerminals()')
+
+if vim.g.neovide then
+   -- Helper function for transparency formatting
+   local alpha = function()
+      return string.format("%x", math.floor(255 * vim.g.transparency or 0.8))
+   end
+   -- g:neovide_transparency should be 0 if you want to unify transparency of content and title bar.
+   vim.g.neovide_transparency = 0.0
+   vim.g.transparency = 0.8
+   vim.g.neovide_background_color = "#0f1117" .. alpha()
+end
